720800b4b535e6c08963b1919c519e7afddaa43d
Nnder <asfahaikrkt6k56@yandex.ru>
2025-09-09 14:06:19 +0300

    init @telegram-apps/create-mini-app

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Section, Cell, Image, List } from "@telegram-apps/telegram-ui";
+
+export default function Home() {
+  return (
+    <div>
+      <div>123</div>
+    </div>
+  );
+}
